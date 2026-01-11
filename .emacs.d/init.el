5eb32dae663ad9f716332e61781e0c032e1f9467
Rituraj Borpujari <mail.riturajborpujari@gmail.com>
2026-01-11 19:27:58 +0530

    colorscheme now matches vim

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,21 +3,13 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-(dolist (pkg '(gruber-darker-theme multiple-cursors go-mode))
+(dolist (pkg '(multiple-cursors go-mode))
   (unless (package-installed-p pkg)
     (package-refresh-contents)
     (package-install pkg)))
 
-(load-theme 'gruber-darker t)
-
-(let ((backup-dir (expand-file-name "backups/" user-emacs-directory))
-      (auto-dir   (expand-file-name "auto-save/" user-emacs-directory)))
-  (make-directory backup-dir t)
-  (make-directory auto-dir t)
-  (setq backup-directory-alist `(("." . ,backup-dir))
-        auto-save-file-name-transforms `((".*" ,auto-dir t))
-        auto-save-list-file-prefix (concat auto-dir ".saves-")
-        create-lockfiles nil))
+(add-to-list 'load-path "~/.emacs.d/lisp")
+(require 'colorscheme)
 
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
@@ -29,7 +21,7 @@
 (scroll-bar-mode -1)
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode t)
-(set-face-attribute 'default nil :font "NotoSansM NFM ExtCondLight-21")
+(set-face-attribute 'default nil :font "NotoSansM NFM ExtCondLight-22")
 (setq-default tab-width 4)
 (global-set-key (kbd "TAB") 'self-insert-command)
 (global-set-key (kbd "M-m") #'compile)
@@ -37,20 +29,22 @@
 (setq c-basic-offset 4)
 (setq indent-tabs-mode nil)
 (setq grep-command "grep -rn ")
-(setq display-buffer-alist
-      '(("\\*compilation\\*"
-         (display-buffer-reuse-window display-buffer-at-bottom))))
 (setq doc-view-continuous 1)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(empv go-mode gruber-darker-theme multiple-cursors)))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+ '(doc-view-svg-face ((t (:background "#181818" :foreground "#efefef")))))
+(defun open-in-zathura ()
+  (interactive)
+  (start-process "zathura" nil "zathura" (buffer-file-name)))
+(global-set-key (kbd "C-c z") #'open-in-zathura)
+(add-to-list 'auto-mode-alist '("\\.pdf\\'" . (lambda ()
+                                                (open-in-zathura)
+                                                (kill-buffer))))
+
+(let ((backup-dir (expand-file-name "backups/" user-emacs-directory))
+      (auto-dir   (expand-file-name "auto-save/" user-emacs-directory)))
+  (make-directory backup-dir t)
+  (make-directory auto-dir t)
+  (setq backup-directory-alist `(("." . ,backup-dir))
+        auto-save-file-name-transforms `((".*" ,auto-dir t))
+        auto-save-list-file-prefix (concat auto-dir ".saves-")
+        create-lockfiles nil))
---
+++ .emacs.d/lisp/colorscheme.el
@@ -0,0 +1,30 @@
+(custom-theme-set-faces
+ 'user
+ 
+ '(default ((t (:background "#1d1d1d" :foreground "#efefef"))))
+ '(font-lock-string-face ((t (:foreground "#30d020"))))
+ '(font-lock-keyword-face ((t (:foreground "#efefef"))))
+ '(font-lock-function-name-face ((t (:foreground "#fdc04b"))))
+ '(font-lock-preprocessor-face ((t (:foreground "#efefef"))))
+ '(font-lock-variable-name-face ((t (:foreground "#efefef"))))
+ '(font-lock-type-face ((t (:foreground "#d0e0d0"))))
+ '(font-lock-constant-face ((t (:foreground "#30d020"))))
+ '(font-lock-builtin-face ((t (:foreground "#efefef"))))
+ '(region ((t (:background "#113366"))))
+ '(mode-line ((t (:background "#242424" :foreground "#efefef" :box nil))))
+ '(mode-line-inactive ((t (:background "#1c1c1c" :foreground "#777777" :box nil))))
+ '(line-number ((t (:foreground "#444444"))))
+ '(line-number-current-line ((t (:foreground "#e5c07b"))))
+ '(minibuffer-prompt ((t (:foreground "#fdc04b"))))
+ '(link ((t (:foreground "#fdc04b" :underline t))))
+ '(link-visited ((t (:foreground "#fdc04b" :underline t)))))
+
+(defun minimal-c-function-call-font-lock ()
+  (font-lock-add-keywords
+   nil
+   '(("\\_<\\([A-Za-z_][A-Za-z0-9_]*\\)\\s-*("
+      1 font-lock-function-name-face))))
+
+(add-hook 'c-mode-hook #'minimal-c-function-call-font-lock)
+
+(provide 'colorscheme)
